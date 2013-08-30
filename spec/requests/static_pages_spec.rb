984954b2e78e72b76fef44500b3420e6b5bec73b
Ikuo Suyama <suppy@hi-ho.ne.jp>
2013-08-30 13:26:43 +0900

    3.5 演習を終了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+ 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -9,8 +12,7 @@ describe "Static Pages" do
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_title(
-                        "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_title("#{base_title} | Home")
     end
   end
 
@@ -23,8 +25,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_title(
-                        "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_title("#{base_title} | Help")
     end
   end
 
@@ -37,8 +38,20 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_title(
-                    "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title("#{base_title} | Contact")
     end
   end
 end
