d6edf5d4b391dfd0355ab30366af0d88e6c7d2c9
Ikuo Suyama <suppy@hi-ho.ne.jp>
2013-09-05 16:04:21 +0900

    5.4　ユーザーのサインアップ：最初のステップ 終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root 'static_pages#home'
+
+  get 'signup' => 'users#new'
+
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
